ece897e08a5fd5f650cbae5f6b6073b7a1596a3c
Vinicius Patricio <viniciuspatricio.33@gmail.com>
2025-06-16 20:45:50 -0300

    Adicionei a paginação pelos próprios componentes do Angular e comecei a inserir os aquivos para a estilização com tailwind

--- src/app/pokemon/pokemon-list/pokemon-list.page.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  IonContent,
-  IonList,
-  IonItem,
-  IonAvatar,
-  IonLabel,
-  IonInfiniteScroll,
-  IonInfiniteScrollContent
-} from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
+import { 
+  IonContent, 
+  IonList, 
+  IonItem, 
+  IonAvatar, 
+  IonLabel,
+  IonButton,
+  IonButtons,
+  IonToolbar,
+  IonCardContent
+} from '@ionic/angular/standalone';
 import { PokemonService } from '../services/pokemon.service';
 import { PokemonBasicInfo } from '../interfaces/pokemon.interface';
 
@@ -22,47 +24,76 @@ import { PokemonBasicInfo } from '../interfaces/pokemon.interface';
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
+    UpperCasePipe,
     IonContent,
+    IonCardContent,
     IonList,
     IonItem,
     IonAvatar,
     IonLabel,
-    IonInfiniteScroll,
-    IonInfiniteScrollContent,
-    RouterModule
+    IonButton,
+    IonButtons,
+    IonToolbar
   ]
 })
 export class PokemonListPage implements OnInit {
   private pokemonService = inject(PokemonService);
   pokemons: PokemonBasicInfo[] = [];
-  loading = true;
+  loading = false;
+  currentPage = 1;
   offset = 0;
-  limit = 20;
+  pageSize = 20;
+  hasMore= true;
+  totalPokemons = 0;
+  totalPages = 0;
 
   ngOnInit(): void {
     this.loadPokemons();
   }
 
-  loadPokemons(loadMore = false): void {
-    if (loadMore) this.offset += this.limit;
+  loadPokemons(): void {
+    if (this.loading) return;
+      
+    this.loading = true;
+    const offset = (this.currentPage - 1) * this.pageSize;
 
-    this.pokemonService.getPokemons(this.limit, this.offset).subscribe({
+    this.pokemonService.getPokemons(this.pageSize, offset)
+    .subscribe({
       next: (response) => {
-        this.pokemons = [...this.pokemons, ...response.results];
+        this.pokemons = response.results;
+        this.totalPokemons = response.count;
+        this.totalPages = Math.ceil(this.totalPokemons / this.pageSize);
         this.loading = false;
       },
-      error: (err) => console.error('Erro ao carregar pokémons:', err),
+      error: (err) => {
+        console.error('Erro ao carregar pokémons:', err);
+        this.loading = false;
+      }
     });
   }
 
-  onInfiniteScroll(ev: Event): void {
-    this.loadPokemons(true);
-    const infiniteScroll = (ev as CustomEvent).target as HTMLIonInfiniteScrollElement;
-    setTimeout(() => infiniteScroll.complete(), 500);
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadPokemons();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPokemons();
+    }
   }
 
   extractId(url: string): string {
     const parts = url.split('/');
     return parts[parts.length - 2];
+  }
+
+  handleImageError(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = '';
   }
 }
